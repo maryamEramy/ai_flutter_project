baad13a35c230aef25858795bd0820dd9e82c624
maryam <eramy.mary@gmail.com>
2025-11-02 13:56:24 +0330

    use speach-to-text package

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -3,6 +3,8 @@ import 'package:ai_flutter_project/ChatService.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -62,8 +64,48 @@ class _ChatScreenState extends State<ChatScreen> {
       flutterTts.setVoice({
         "name": "ur-pk-x-cfn-network" , "locale": "ur-PK"
       });
-
   }
+
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+      print(_lastWords);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,6 +135,12 @@ class _ChatScreenState extends State<ChatScreen> {
                     controller: textEditingController,
                     decoration: InputDecoration(hintText: "write here..."),
                   ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _startListening();
+                  },
+                  icon: Icon(Icons.mic),
                 ),
                 IconButton(
                   onPressed: () {
